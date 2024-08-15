6027a6d32010f6ceb643152ce24d5fdad3d06c3a
ewaysolution <help.eway.solutions@gmail.com>
2024-08-15 12:59:06 +0400

    added style

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ $orders = $orderController->handleRequest();
     </div>
 
     <footer>
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>&copy; 2024 EyemazyStore. All rights reserved.</p>
         <p><a href="/privacy-policy">Privacy Policy</a> | <a href="/contact">Contact Us</a></p>
     </footer>
 </body>
